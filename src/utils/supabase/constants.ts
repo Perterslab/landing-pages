7f0ae109af1c07c6e7e2c13b037819887c7a2bb1
Peterslab <peterstudioskn@gmail.com>
2026-04-24 17:14:13 +0700

    fix:数据库

--- src/utils/supabase/constants.ts
+++ src/utils/supabase/constants.ts
@@ -1,3 +1,3 @@
-export const SUPABASE_URL = "https://iwdfzvfqbtokqetmbmbp.supabase.co";
+export const SUPABASE_URL = "https://jdnuikgtxooetvoyyyvw.supabase.co";
 export const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDU2NzAxMCwiZXhwIjoxOTQ2MTQzMDEwfQ._gr6kXGkQBi9BM9dx5vKaNKYj_DJN1xlkarprGpM_fU";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpkbnVpa2d0eG9vZXR2b3l5eXZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5MTUzNDQsImV4cCI6MjA5MjQ5MTM0NH0.QcBdJ4IVSaQQJn1y7s8RSRiR0yqcdtfFp2F1IZSNE2E";
